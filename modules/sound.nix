f659c102ae1d73c406c4bd659253da29b712dbd3
Junior Sundar <juniorsundar@gmail.com>
2025-03-01 20:34:29 +0400

    use pipewire instead

--- modules/sound.nix
+++ modules/sound.nix
@@ -1,12 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = false;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
+    # hardware.pulseaudio.enable = true;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        jack.enable = true;
+    };
 }
