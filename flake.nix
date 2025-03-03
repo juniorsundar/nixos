687420f0d437a020be641975f4c09244be370c2e
Junior Sundar <juniorsundar@gmail.com>
2025-03-03 22:26:39 +0400

    Updated to unstable branch and added additional mount points

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
   };
 
--- modules/boot.nix
+++ modules/boot.nix
@@ -9,9 +9,21 @@
     efi.canTouchEfiVariables = true;
   };
 
-  fileSystems."/mnt/storage" = {
+  fileSystems."/mnt/drive_01" = {
     device = "/dev/disk/by-uuid/22d7d5c8-a400-4657-8d37-f0ad61531f5c";
     fsType = "ext4";
+    options = ["defaults" "noauto" "x-systemd.automount" "nofail"];
+  };
+
+  fileSystems."/mnt/drive_02" = {
+    device = "/dev/disk/by-uuid/C4E46B98E46B8B8C";
+    fsType = "ntfs-3g";
+    options = ["defaults" "noauto" "x-systemd.automount" "nofail"];
+  };
+
+  fileSystems."/mnt/drive_03" = {
+    device = "/dev/disk/by-uuid/9CE87958E879319E";
+    fsType = "ntfs-3g";
     options = ["defaults" "noauto" "x-systemd.automount" "nofail"];
   };
 
