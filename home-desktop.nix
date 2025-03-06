72fcbad5aedba918199074312bc3947fa8bf9219
Junior Sundar <juniorsundar@gmail.com>
2025-03-06 22:41:29 +0400

    Updated dotfiles

--- home-desktop.nix
+++ home-desktop.nix
@@ -2,13 +2,7 @@
   config,
   pkgs,
   ...
-}: let
-  stowDirs = [
-    kitty
-    nvim
-  ];
-  dotfilesPath = "/etc/nixos/dotfiles"; # Absolute path to your dotfiles
-in {
+}: {
   home.username = "juniorsundar"; # Replace with your actual username
   home.homeDirectory = "/home/juniorsundar"; # Adjust for macOS (/Users/username)
   home.stateVersion = "24.11";
@@ -18,32 +12,16 @@ in {
     git
   ];
 
-  home.activation.stowDotfiles = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    echo "🚀 Stowing dotfiles from ${dotfilesPath}"
+  xdg.configFile = {
+    "nvim".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/nvim/.config/nvim";
+    "doom".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/emacs/.config/doom";
+    "wofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/wofi/.config/wofi";
+    "hypr".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/hypr/.config/hypr";
+    "swaync".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/swaync/.config/swaync";
+  };
 
-    # Create backup directory with timestamp
-    export STOW_BACKUP="$HOME/.stow-backup/$(date +%Y%m%d-%H%M%S)"
-    mkdir -p "$STOW_BACKUP"
-
-    ${pkgs.stow}/bin/stow \
-      --verbose=3 \
-      --dir="${dotfilesPath}" \
-      --target="$HOME" \
-      --backup="$STOW_BACKUP" \
-      --adopt \
-      ${toString stowDirs}
-
-    echo "✅ Stow complete - backups stored in: $STOW_BACKUP"
-  '';
-
-  # Disable Home Manager management of stowed files
-  home.file = {
-    # ".config/alacritty".enable = false;
-    # ".config/awesome".enable = false;
-    # ".bashrc".enable = false;
-    ".config/nvim" = false;
-    ".config/kitty" = false;
-    # Add similar entries for other managed directories/files
+  xdg.dataFile = {
+    
   };
 
   # Configure programs
