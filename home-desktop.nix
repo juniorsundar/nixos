3b3636d4521f2a4384c47f9594d1de9026f8de2a
Junior Sundar <juniorsundar@gmail.com>
2025-03-18 21:49:26 +0400

    Removed dotfiles
    test

--- home-desktop.nix
+++ home-desktop.nix
@@ -14,6 +14,7 @@
     ntfs3g
 
     wl-clipboard
+
     grim
     mosh
     waybar
@@ -25,45 +26,6 @@
     pavucontrol
     rofi-wayland
   ];
-
-  home.file = {
-    ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/git/.gitconfig";
-    ".p10k.zsh".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.p10k.zsh";
-    ".themes".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/gtk/.themes";
-    ".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/wezterm/.wezterm.lua";
-    ".zoxide.zsh".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.zoxide.zsh";
-    ".zsh_aliases".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.zsh_aliases";
-    ".zsh_completions".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.zsh_completions";
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.zshrc";
-    ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/tmux/.tmux.conf";
-    ".tmux".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/tmux/.tmux";
-  };
-
-  xdg = {
-    configFile = {
-      "bat".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/bat/.config/bat";
-      "btop".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/btop/.config/btop";
-      "delta".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/delta/.config/delta";
-      "doom".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/emacs/.config/doom";
-      "gtk-2.0".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/gtk/.config/gtk-2.0";
-      "gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/gtk/.config/gtk-3.0";
-      "gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/gtk/.config/gtk-4.0";
-      "kitty".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/kitty/.config/kitty";
-      "lazygit".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/lazygit/.config/lazygit";
-      "mako".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/mako/.config/mako";
-      "nvim".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/nvim/.config/nvim";
-      "rofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/rofi/.config/rofi";
-      "waybar".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/waybar/.config/waybar";
-      "wofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/wofi/.config/wofi";
-      "zellij".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zellij/.config/zellij";
-      "zsh-abbr".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/zsh/.config/zsh-abbr";
-    };
-
-    dataFile = {
-      "rofi".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/rofi/.local/share/rofi";
-      "fonts".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/dotfiles/fonts/.local/share/fonts";
-    };
-  };
 
   wayland.windowManager.hyprland = {
     enable = true;
