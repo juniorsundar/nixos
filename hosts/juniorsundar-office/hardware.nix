e9b5f2c608d3a3b0544cdb4c5c1b1a9ec2c36d63
Junior Sundar <juniorsundar@gmail.com>
2025-09-09 13:14:28 +0400

    Added office configuration

---
+++ hosts/juniorsundar-office/hardware.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }: {
+  imports = [ ./hardware-autogen.nix ];
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  hardware = {
+#    nvidia = {
+#      open = false;
+      # prime = {
+      #   offload = {
+      #     enable = true;
+      #     enableOffloadCmd = true;
+      #   };
+      #   amdgpuBusId = "PCI:6:0:0";
+      #   nvidiaBusId = "PCI:1:0:0";
+      # };
+#      modesetting.enable = true;
+#      powerManagement = {
+#        enable = true;
+#        finegrained = false;
+#      };
+#      nvidiaSettings = true;
+#      package = config.boot.kernelPackages.nvidiaPackages.stable;
+#    };
+
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+
+#    nvidia-container-toolkit.enable = false;
+  };
+}
