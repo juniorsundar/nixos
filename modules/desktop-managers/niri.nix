4aa54fdf1bbe233a7dd1936f215bd3123e78578c
Junior Sundar <juniorsundar@gmail.com>
2025-09-15 23:11:40 +0400

    Added niri

---
+++ modules/desktop-managers/niri.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }: {
+  programs.niri = {
+    enable = true;
+  };
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.swaylock = {};
+
+  programs.waybar.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    playerctl
+    pavucontrol
+
+    waylock
+    mako
+    swayidle
+    rofi
+
+    xorg.xhost
+  ];
+}
+
