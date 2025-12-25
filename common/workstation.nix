8f8ee307d86d09f039ba8d2772fd9252492f06bb
Junior Sundar <juniorsundar@gmail.com>
2025-12-25 12:39:14 +0400

    Modularisation

---
+++ common/workstation.nix
@@ -0,0 +1,64 @@
+{ config, pkgs, ... }:
+{
+  # User Configuration
+  users = {
+    users = {
+      juniorsundar = (import ../users/common-system.nix) { inherit pkgs; };
+    };
+    extraGroups.docker.members = [ "juniorsundar" ];
+  };
+
+  # Boot settings
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  # Services
+  services = {
+    blueman.enable = true;
+
+    syncthing = {
+      enable = true;
+      dataDir = "/home/juniorsundar/Dropbox/";
+      openDefaultPorts = true;
+      configDir = "/home/juniorsundar/.config/syncthing";
+      user = "juniorsundar";
+      group = "users";
+      guiAddress = "0.0.0.0:8384";
+    };
+
+    libinput.enable = true;
+    xserver = {
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      videoDrivers = [ "nvidia" ];
+    };
+  };
+
+  # Environment
+  environment = {
+    shells = with pkgs; [
+      zsh
+      bash
+    ];
+    
+    systemPackages = with pkgs; [
+      # Important stuff
+      clang
+      gcc
+      psmisc
+      python3
+      glibc
+      nodejs
+      uv
+      direnv
+      # fileSystems
+      ntfs3g
+      mosh
+      # App Suites
+      libreoffice
+      vscode
+      tree-sitter
+    ];
+  };
+}
--- users/personal/mac-home.nix
+++ users/personal/mac-home.nix
@@ -1,54 +1,18 @@
+{ pkgs, ... }:
 {
-  config,
-  pkgs,
-  inputs,
-  ...
-}:
-{
-  home.username = "juniorsundar"; # Replace with your actual username
+  imports = [
+    ../modules/emacs.nix
+  ];
+
+  home.username = "juniorsundar";
   home.homeDirectory = pkgs.lib.mkForce "/Users/juniorsundar";
   home.stateVersion = "25.11";
 
   # Install packages
   home.packages = with pkgs; [
-    emacs-lsp-booster
     coreutils
   ];
   fonts.fontconfig.enable = true;
 
-  programs = {
-    emacs = {
-      enable = true;
-      package = pkgs.emacs-git;
-      extraPackages = epkgs: [
-        (epkgs.treesit-grammars.with-grammars (
-          grammars: with grammars; [
-            tree-sitter-bash
-            tree-sitter-c
-            tree-sitter-cpp
-            tree-sitter-dockerfile
-            tree-sitter-go
-            tree-sitter-html
-            tree-sitter-javascript
-            tree-sitter-json
-            tree-sitter-lua
-            tree-sitter-markdown
-            tree-sitter-nix
-            tree-sitter-python
-            tree-sitter-rust
-            tree-sitter-toml
-            tree-sitter-tsx
-            tree-sitter-typescript
-            tree-sitter-yaml
-            tree-sitter-nix
-          ]
-        ))
-        epkgs.vterm
-      ];
-    };
-    home-manager = {
-      enable = true;
-    };
-  };
-
+  programs.home-manager.enable = true;
 }
