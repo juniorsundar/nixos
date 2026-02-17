6ff7cf0b2e4d93ea7b1cf9e0980a3f9de6623916
Junior Sundar <juniorsundar@gmail.com>
2026-02-17 09:32:12 +0400

    Add cachix for faster builds

--- flake.nix
+++ flake.nix
@@ -37,6 +37,16 @@
     };
 
     wezterm.url = "github:wezterm/wezterm?dir=nix";
+
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   outputs =
