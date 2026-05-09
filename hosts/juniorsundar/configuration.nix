756b2096c2131a64c8adb32605203c23e5422192
Junior Sundar <juniorsundar@gmail.com>
2026-05-09 10:31:28 +0400

    Add distrobox

--- hosts/juniorsundar/configuration.nix
+++ hosts/juniorsundar/configuration.nix
@@ -19,4 +19,8 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  environment.systemPackages = with pkgs; [
+    distrobox
+  ];
 }
